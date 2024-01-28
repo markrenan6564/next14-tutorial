12cbc83ad321502c58fdc07d47a2d5148abe8204
Mark Renan <markrenan.21@gmail.com>
2024-01-28 16:02:29 +0800

    feat: contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import styles from "./contact.module.css";
+import Image from "next/image";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.image_container}>
+        <Image
+          src="/contact.png"
+          alt="Contact image"
+          fill
+          className={styles.contact_image}
+        />
+      </div>
+      <div className={styles.form_container}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea placeholder="Message"></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
